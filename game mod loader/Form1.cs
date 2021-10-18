8c076f98bf8eaa1bcc179434230136645888144f
USBMenus <69381115+USBMenus@users.noreply.github.com>
2021-10-18 11:34:54 -0500

    fix

--- game mod loader/Form1.cs
+++ game mod loader/Form1.cs
@@ -27,37 +27,58 @@ namespace game_mod_loader
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Write Data6.pak
-            byte[] file1 = Properties.Resources.Data6;
-            File.WriteAllBytes(textBox1.Text + "\\DW\\Data6.pak", file1);
-            //Write dsound.dll
-            byte[] file3 = Properties.Resources.dsound;
-            File.WriteAllBytes(textBox1.Text + "\\dsound.dll", file3);
-            //Create dide_mod.ini
-            using (StreamWriter sw = new StreamWriter(textBox1.Text + "\\dide_mod.ini"))
+            if (Directory.Exists(textBox1.Text))
             {
-                sw.Write("[General]\nEnableMod = 1\nEnableLogging = 0\nLogFile = dide_mod_log.ini\n[Features]\nDeveloperMenu = 0\nCustomPak = 1\n[CustomPak]\n");
-                sw.WriteLine(textBox1.Text + "\\DW\\Data6.pak=1");
+                //Write Data6.pak
+                byte[] file1 = Properties.Resources.Data6;
+                File.WriteAllBytes(textBox1.Text + "\\DW\\Data6.pak", file1);
+                //Write dsound.dll
+                byte[] file3 = Properties.Resources.dsound;
+                File.WriteAllBytes(textBox1.Text + "\\dsound.dll", file3);
+                //Create and Write dide_mod.ini
+                using (StreamWriter sw = new StreamWriter(textBox1.Text + "\\dide_mod.ini"))
+                {
+                    sw.Write("[General]\nEnableMod = 1\nEnableLogging = 0\nLogFile = dide_mod_log.ini\n[Features]\nDeveloperMenu = 0\nCustomPak = 1\n[CustomPak]\n");
+                    sw.WriteLine(textBox1.Text + "\\DW\\Data6.pak=1");
+                }
+                MessageBox.Show("Successfully Loaded!", "Mod Loaded", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
-            MessageBox.Show("Successfully Loaded!", "Mod Loaded", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else 
+            {
+                MessageBox.Show("This directory does not exist!", "Invalid Directory!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //Delete Data6.pak
-            File.Delete(textBox1.Text + "\\DW\\Data6.pak");
-           //Delete dide_mod.ini
-            File.Delete(textBox1.Text + "\\dide_mod.ini");
-            //Delete dsound.dll
-            File.Delete(textBox1.Text + "\\dsound.dll");
-            MessageBox.Show("Successfully Unloaded!", "Mod Unloaded", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            if (Directory.Exists(textBox1.Text))
+            {
+                //Delete Data6.pak
+                File.Delete(textBox1.Text + "\\DW\\Data6.pak");
+                //Delete dide_mod.ini
+                File.Delete(textBox1.Text + "\\dide_mod.ini");
+                //Delete dsound.dll
+                File.Delete(textBox1.Text + "\\dsound.dll");
+                MessageBox.Show("Successfully Unloaded!", "Mod Unloaded", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                MessageBox.Show("This directory does not exist!", "Invalid Directory!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //Launch Dying Light
-            MessageBox.Show("Launching Dying Light", "Launching...", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            Process.Start(textBox1.Text + "\\DyingLightGame.exe");
+            if (Directory.Exists(textBox1.Text))
+            {
+                //Launch Dying Light
+                MessageBox.Show("Launching Dying Light", "Launching...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Process.Start(textBox1.Text + "\\DyingLightGame.exe");
+            }
+            else
+            {
+                MessageBox.Show("This directory does not exist!", "Invalid Directory!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
